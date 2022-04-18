aad3f141afd549531c5244b30432fe1199fd04da
Ubuntu <ubuntu@ip-172-31-2-76.ec2.internal>
2022-04-18 16:50:25 +0000

    added program

---
+++ positives_negatives.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Created by: Noah McCaskill
+# Created on: April 2022
+# This is a integer, positive, negative, or zero program
+
+
+def main():
+
+    # This i
+
+    # input
+    user_number = int(input("Enter your number here! : "))
+
+    # process & output
+    if user_number > 0:
+        print("\nyour number  is positive!")
+    elif user_number < 0:
+        print("\nYour number is negative!")
+    else:
+        print("\nYour number is zero!")
+    print("\nDone!")
+
+
+if __name__ == "__main__":
+    main()
+    
